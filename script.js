d7d91ccaef767c13a47aea15dbb6d2cc6d2a3c81
kowalskimariusz <48931158+kowalskimariusz@users.noreply.github.com>
2019-04-27 23:21:27 +0200

    New Tests
    Added tests for: non-numeric, negative number and non-integer arguments.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function fib(num){
+    if (typeof num !== "number") {
+        throw TypeError("Argument musi być liczbą");
+    } else if (num < 0 || Number(num.toFixed(0)) !== num){
+        throw RangeError("Argument musi być dodatnią liczbą całkowitą");
+    }
     return num < 2 ? 1 : num * fib(num - 1);
 }
 
--- script.test.js
+++ script.test.js
@@ -4,3 +4,16 @@ const fib = functions_to_test.fib;
 test('check if fib(3) = 6',()=>{
 	expect(fib(3)).toBe(6);
 });
+
+test('check if fib with non-numeric argument throws an error',()=>{
+	["abc", true, Symbol("z"), [], {}, ()=>{}, new Map(), new Set(), new Error()]
+		.forEach(el => expect(()=>fib(el)).toThrowError(TypeError("Argument musi być liczbą")));
+});
+
+test('check if fib with negative number argument throws an error',()=>{
+	expect(()=>fib(-1)).toThrowError(RangeError("Argument musi być dodatnią liczbą całkowitą"));
+});
+
+test('check if fib with non-integer number argument throws an error',()=>{
+	expect(()=>fib(3.65)).toThrowError(RangeError("Argument musi być dodatnią liczbą całkowitą"));
+});
